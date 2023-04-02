114cfb5b0669cc928785ad15ed6f02167567d9ce
matthewmonolith <matthew.page2016@outlook.com>
2023-04-02 13:32:56 +0100

    adding another kata

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+// 8 kyu
+// Grasshopper - Messi goals function
+// Messi goals function
+// Messi is a soccer player with goals in three leagues:
+
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
+
+// Note: the input will always be valid.
+
+// For example:
+
+// 5, 10, 2  -->  17
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+
+
+
+
 //7 kyu Reverse words
 // Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
 
