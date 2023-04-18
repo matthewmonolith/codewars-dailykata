298a63de655ec5198fc6934706c35b5e7d99dcaa
matthewmonolith <matthew.page2016@outlook.com>
2023-04-18 18:20:30 +0100

    adding commit, 18th April

---
+++ js/2023-4-18.js
@@ -0,0 +1,7 @@
+//function that returns the numbers inbetween a and b, b always greater than a
+function between(a, b) {
+    let array = [];
+    for(let i = a; i <= b; i++){
+      array.push(i);
+    }return array
+  }
