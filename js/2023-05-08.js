3ca3777d1342590648fd39e16187b30d9a16bd59
matthewmonolith <matthew.page2016@outlook.com>
2023-05-08 18:21:35 +0100

    7kyu 8th May

---
+++ js/2023-05-08.js
@@ -0,0 +1,6 @@
+// write the function isAnagram
+var isAnagram = function(test, original) {
+    var t = test.toLowerCase().split('').sort().join('');
+    var o = original.toLowerCase().split('').sort().join('');
+    return (t==o)?true:false;
+  };
