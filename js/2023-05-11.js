ada034880eac506888319b05e5f3a14d70671835
matthewmonolith <matthew.page2016@outlook.com>
2023-05-13 17:17:53 +0100

    adding 8kyu that uses a decreasing iteration normally do positive

--- js/2023-05-11.js
+++ js/2023-05-11.js
@@ -3,3 +3,9 @@ function duplicateCount(text){
       return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
     }).length;
   }
+
+  
+
+// The condition in the anonymous function checks if the current value val occurs more than once in the array arr. It does this by checking if the index of the current value is not equal to its last index (i.e., it occurs more than once), and that its last index is equal to the current index (i.e., it is the last occurrence of that value in the array).
+// .length returns the number of elements in the filtered array, which corresponds to the number of characters that occur more than once in the original string.
+// For example, if the input string is "Hello, world!", the function would return 3, because the characters 'l', 'o', and ' ' occur more than once in the string.
