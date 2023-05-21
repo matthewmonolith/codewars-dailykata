2e2d40b5b39a21ed5eb9c9603b86449cd7189c68
matthewmonolith <matthew.page2016@outlook.com>
2023-05-21 17:43:22 +0100

    6kyu was maths so had to look up solution

---
+++ js/2023-05-21.js
@@ -0,0 +1,10 @@
+//6kyu find out X when M and N are known
+function checkchoose(m, n) {
+    let x = 1;
+    for (let i = 1; i < n; i++) {
+      x = Math.round(x * (n + 1 - i) / i)
+      if (x == m) 
+        return i;
+    }
+    return -1;
+  }
