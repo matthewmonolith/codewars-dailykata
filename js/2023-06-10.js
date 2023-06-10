57e35813a8aa15de9b5a8e2d78ed32f04ae6cc54
matthewmonolith <matthew.page2016@outlook.com>
2023-06-10 16:16:17 +0100

    6kyu

---
+++ js/2023-06-10.js
@@ -0,0 +1,18 @@
+function travel(r, zipcode) {
+    const addressList = r.split(",");
+
+    const streets = [];
+    const numbers = [];
+
+    addressList.forEach(address => {
+        let zip = address.substring(address.length - 8, address.length);
+        if (zip === zipcode) {
+            let number = address.substring(0, address.indexOf(" ") + 1);
+            let street = address.substring(address.indexOf(" "), address.length -8);
+            
+            streets.push(street.trim());
+            numbers.push(number.trim());
+        }
+    });
+    return `${zipcode}:${streets.join(",")}/${numbers.join(",")}`;
+}
