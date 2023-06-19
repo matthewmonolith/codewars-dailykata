f8c610ccf142ab90edb00f85f6eee51c0af08992
matthewmonolith <matthew.page2016@outlook.com>
2023-06-19 16:30:30 +0100

    7kyu

---
+++ js/2023-06-19.js
@@ -0,0 +1,13 @@
+function likeOrDislike(buttons) {
+    let state = 'Nothing';
+  
+    for (let i = 0; i < buttons.length; i++) {
+      if (buttons[i] === state) {
+        state = 'Nothing'
+      } else {
+        state = buttons[i]
+      }
+    }
+  
+    return state
+  }
