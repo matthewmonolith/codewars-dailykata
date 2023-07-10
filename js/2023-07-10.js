7ac5746307ee6fb81cbae6f9d377ae0d2052a3e7
matthewmonolith <matthew.page2016@outlook.com>
2023-07-10 10:50:23 +0100

    7kyu arrays

---
+++ js/2023-07-10.js
@@ -0,0 +1,10 @@
+const dominator = arr => {
+    let count  = {};
+    arr.forEach(el => {
+      count[el] ? count[el]++ : count[el] = 1;
+    })
+    for (let k in count) {
+      if (count[k] > arr.length/2) return +k
+    }
+    return -1;
+ }
