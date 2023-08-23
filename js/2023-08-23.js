9508b4a0c987fdfebdb0f5d20bd75195feb3db3a
matthewmonolith <matthew.page2016@outlook.com>
2023-08-23 15:28:31 +0100

    7kyu strings

--- js/2023-08-23.js
+++ js/2023-08-23.js
@@ -1,14 +1,4 @@
-var number = function(busStops){
-    let left = 0
-    for(let i = 0; i < busStops.length;i++){
-      left+=busStops[i][0]
-      left-=busStops[i][1]
-    }
-    return left
-  }
-
-
-  function longest(sentence){
+function longest(sentence){
     const arr = sentence.split(' ')
     let longest = arr[0];
     for(let i = 0; i < arr.length; i++){
