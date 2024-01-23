0ae298d3f5b019d6c332f893afd76ad13c824036
matthewmonolith <matthew.page2016@outlook.com>
2024-01-23 18:35:23 +0000

    maths performance kyu

---
+++ js/2024-1-23.js
@@ -0,0 +1,5 @@
+function oddCount(n) {
+    return Math.floor(n / 2);
+  }
+
+  //In any range from 0 to n, half of the numbers are odd. So, you can simply divide n by 2 and get the count of odd numbers.
