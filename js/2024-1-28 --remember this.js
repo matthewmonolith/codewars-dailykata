d7d3f1ae228c15f5dfbce4afbccb56328de49d86
matthewmonolith <matthew.page2016@outlook.com>
2024-01-29 18:14:37 +0000

    arrays

--- js/2024-1-28 --remember this.js
+++ js/2024-1-28 --remember this.js
@@ -1,9 +1,3 @@
-function wordsToMarks(string){
-    const alphabetArray = Array.from({ length: 26 }, (_, i) => String.fromCharCode(97 + i));
-    let res = 0
-    const arr = string.split('')
-    for(let i = 0; i < arr.length; i++){
-      res = res + alphabetArray.indexOf(arr[i]) + 1
-    }
-    return res
+function wordsToMarks(string) {
+    return string.split('').reduce((total, char) => total + char.charCodeAt(0) - 96, 0);
   }
