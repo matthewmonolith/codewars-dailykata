5591cf47ce67bd71ebc0d30faf22b6cf286f1768
matthewmonolith <matthew.page2016@outlook.com>
2024-02-10 18:09:34 +0000

    array objs recap

---
+++ js/2024-02-10.js
@@ -0,0 +1,9 @@
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
+
+let arr = [ john, pete, mary ];
+
+function getAverageAge(arr){
+  return arr.reduce((acc, c) => acc.age + c, 0)/arr.length
+}
